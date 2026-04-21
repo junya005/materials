e70d4723254c6e7e501df955028a7d34cd7626e6
Junya Kobayashi <168046274+junya005@users.noreply.github.com>
2026-04-21 10:51:12 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://junya005.github.io',
-  base: '/materials',
+  site: 'https://junya005.github.io/materials',
 });
